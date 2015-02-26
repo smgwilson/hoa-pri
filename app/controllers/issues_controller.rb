61241fd08c3a08156f7db02d48c064fde0bf3e68
Sheri Wilson <smgwilson37@gmail.com>
2015-02-25 20:21:22 -0800

    fixes routing for new issues form

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,6 +1,9 @@
 class IssuesController < ApplicationController
 
   def new
+  end
 
+  def create
+    render plain: params[:article].inspect
   end
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,10 @@
 class Issue < ActiveRecord::Base
   has_many :notes
+
+  def create
+    @issue = Isssue.new(params:[:issue])
+
+    @issue.save
+    redirect_to @issue
+  end
 end
