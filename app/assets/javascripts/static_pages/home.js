6a9952f3c76af9c3f7229f98f7666570199eebc8
Sheri Wilson <smgwilson37@gmail.com>
2015-07-09 16:55:09 -0700

    moves javascript out of verb

---
+++ app/assets/javascripts/static_pages/home.js
@@ -0,0 +1,25 @@
+//carousel script
+$(document).ready(function(){
+    // Activate Carousel
+    $("#myCarousel").carousel();
+
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#myCarousel").carousel(2);
+    });
+
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#myCarousel").carousel("next");
+    });
+});
+
